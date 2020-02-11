c20bc8220e5a14eb8304d5cd4cffdbe74ca757fb
TR <timriedesel@me.com>
2020-02-11 00:57:35 -0500

    Add modal View with Labels and DONE Button

--- FuelTripV3/Controller/SettingsViewController.swift
+++ FuelTripV3/Controller/SettingsViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var btnDoneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        popUpView.layer.cornerRadius = 10
+        popUpView.layer.masksToBounds = true
+        btnDoneButton.layer.cornerRadius = 5
+        btnDoneButton.layer.masksToBounds = true
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
     }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
